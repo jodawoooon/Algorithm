1c225a4d4e8a08687bf6501396e583318bd1a822
jodawoooon <38212743+jodawoooon@users.noreply.github.com>
2021-04-04 21:49:29 +0900

    2021-04-04

---
+++ BojStep_new/src/boj/수학/Main_BJ_12907_동물원.java
@@ -0,0 +1,22 @@
+package boj.수학;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_BJ_12907_동물원 {
+	
+	static int N;
+	public static void main(String[] args) throws Exception{
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int n = 1; n <= N; n++) {
+			//동물은 토끼나 고양이.
+			//자신보다 키가 큰 동물의 수를 말해준다.
+			
+		}
+	}
+}
