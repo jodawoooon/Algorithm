24b46044b4fc66ed639490a1234bf0527fc513ea
조다운 <38212743+jodawoooon@users.noreply.github.com>
2021-06-08 17:25:49 +0900

    2021-06-08 bj 7287 등록

---
+++ BojStep_new/src/boj/구현/Main_BJ_7287_B5_등록.java
@@ -0,0 +1,15 @@
+package boj.구현;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main_BJ_7287_B5_등록 {	
+
+	static String N;
+	static int ans;
+	static int[] check;
+	public static void main(String[] args) throws Exception {
+		System.out.println(249);
+		System.out.println("jodawoooon");
+	}
+}
