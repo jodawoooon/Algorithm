ecc010f4e1d77b2d1e26ee4ae96a08c514539edf
조다운 <38212743+jodawoooon@users.noreply.github.com>
2021-06-08 17:44:20 +0900

    2021-06-08 bj 1292 쉽게푸는문제

---
+++ BojStep_new/src/boj/구현/Main_BJ_1292_S5_쉽게푸는문제.java
@@ -0,0 +1,38 @@
+package boj.구현;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_BJ_1292_S5_쉽게푸는문제 {
+	
+	static int A,B,sum;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		A = Integer.parseInt(st.nextToken());
+		B = Integer.parseInt(st.nextToken());
+		
+	
+		int sumA = 0;
+		int sumB = 0;
+		
+		int cnt = 0;
+		int tgt = 1;
+		for (int i = 1; i <= B; i++) {
+			for (int j = 0; j < i; j++) {
+				cnt++;
+				
+				if(cnt>=A) {
+					sum += i;
+				}
+				if(cnt==B) {
+					System.out.println(sum);
+					return;
+				}
+			}
+		}
+		
+	}
+}
