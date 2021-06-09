6496c1b9edac27f8f3fa1b4f1969718e663c81a1
조다운 <38212743+jodawoooon@users.noreply.github.com>
2021-06-09 13:01:23 +0900

    code : programmers K번째 수

---
+++ Programmers/src/level1/Solution_K번째수.java
@@ -0,0 +1,25 @@
+package level1;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Solution_K번째수 {
+	public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        
+        for (int c = 0; c < commands.length; c++) {
+        	int i = commands[c][0];
+			int j = commands[c][1];
+			int k = commands[c][2];
+			
+			ArrayList<Integer> list = new ArrayList<>();
+	        
+			for (int x = i-1; x < j; x++) {
+				list.add(array[x]);
+			}
+			Collections.sort(list);
+			answer[c]=list.get(k-1);
+		}
+        return answer;
+    }
+}
