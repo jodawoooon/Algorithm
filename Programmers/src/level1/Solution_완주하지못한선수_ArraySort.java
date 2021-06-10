41fdcbbdc29ff794e9544036f130fcbb419860ab
조다운 <38212743+jodawoooon@users.noreply.github.com>
2021-06-10 10:39:42 +0900

    code : Programmers 완주하지못한선수 ArraySort

---
+++ Programmers/src/level1/Solution_완주하지못한선수_ArraySort.java
@@ -0,0 +1,26 @@
+package level1;
+
+import java.util.*;
+
+public class Solution_완주하지못한선수_ArraySort {
+	public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        
+        //participant : 참가자 이름
+        //completion : 완주자 이름
+        //참가자 중에는 동명이인이 있을 수 있습니다.
+        //단 한 명의 선수를 제외하고는 모든 선수가 마라톤을 완주하였습니다. => 한명만 찾으면됨
+        
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+        
+        int i=0;
+        for (i = 0; i < completion.length; i++) {
+			if(!completion[i].equals(participant[i])) {
+				return participant[i];
+			}
+		}
+        
+        return participant[i];
+    }
+}
