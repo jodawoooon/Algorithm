48bbdd32d07328602ba8b41b547f55c68f2fc211
조다운 <38212743+jodawoooon@users.noreply.github.com>
2021-06-10 11:28:37 +0900

    code : Programmers 완주하지못한선수 Hash

--- Programmers/src/level1/Solution_완주하지못한선수_Hash.java
+++ Programmers/src/level1/Solution_완주하지못한선수_Hash.java
@@ -11,15 +11,27 @@ public class Solution_완주하지못한선수_Hash {
         //참가자 중에는 동명이인이 있을 수 있습니다.
         //단 한 명의 선수를 제외하고는 모든 선수가 마라톤을 완주하였습니다. => 한명만 찾으면됨
         
-        Arrays.sort(participant);
-        Arrays.sort(completion);
-        
-        for (int i = 0; i < completion.length; i++) {
-			if(!completion[i].equals(participant[i])) {
-				return participant[i];
+        HashMap<String, Integer> map = new HashMap<>();
+        for (String p : participant) {
+			if(map.get(p)==null) {
+				map.put(p, 1);
+			}else { //이름중복일경우
+				map.put(p, map.get(p)+1);
 			}
 		}
         
-        return participant[participant.length-1];
+        for (String p : completion) {
+        	if(map.get(p)!=0) {
+        		map.put(p, map.get(p)-1);
+        	}
+		}
+        
+        for (String key : map.keySet()) {
+			if(map.get(key)!=0) {
+				return key;
+			}
+		}
+        
+        return answer;
     }
 }
