416c03c4b01c002b09b9fb7070fa48b0342acd2d
조다운 <38212743+jodawoooon@users.noreply.github.com>
2021-06-10 09:56:19 +0900

    code : Programmers 스킬체크 level1

--- Programmers/src/level1/Solution_스킬체크테스트2.java
+++ Programmers/src/level1/Solution_스킬체크테스트2.java
@@ -2,14 +2,16 @@ package level1;
 
 public class Solution_스킬체크테스트2 {
 	//단어 s의 가운데 글자를 반환하는 함수, solution을 만들어 보세요.
-	static String answer;
+	
 	public String solution(String s) {
+		StringBuilder sb = new StringBuilder();
+		
 		if(s.length()%2==0) {
-			answer += s.charAt(s.length()/2-1);
-			answer += s.charAt(s.length()/2);
+			sb.append(s.charAt(s.length()/2-1));
+			sb.append(s.charAt(s.length()/2));
 		}else {
-			answer += s.charAt(s.length()/2);
+			sb.append(s.charAt(s.length()/2));
 		}
-        return answer;
+        return sb.toString();
     }
 }
