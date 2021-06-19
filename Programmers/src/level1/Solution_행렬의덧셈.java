78dca93d25659f5b0fe903290fa4e3279f135b4c
조다운 <38212743+jodawoooon@users.noreply.github.com>
2021-06-19 23:18:39 +0900

    code : programmers 행렬의덧셈 arr

---
+++ Programmers/src/level1/Solution_행렬의덧셈.java
@@ -0,0 +1,14 @@
+package level1;
+
+public class Solution_행렬의덧셈 {
+	public int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = new int[arr1.length][arr1[0].length];
+        
+        for (int i = 0; i < answer.length; i++) {
+			for (int j = 0; j < answer[i].length; j++) {
+				answer[i][j] = arr1[i][j] + arr2[i][j];
+			}
+		}
+        return answer;
+    }
+}
