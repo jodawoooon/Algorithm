3cbcdd6b9870c3996a847c262b4a66078e515713
조다운 <38212743+jodawoooon@users.noreply.github.com>
2021-06-19 23:27:47 +0900

    code : Programmers 수박수박수 String

---
+++ Programmers/src/level1/Solution_수박수박수박수.java
@@ -0,0 +1,12 @@
+package level1;
+
+public class Solution_수박수박수박수 {
+	public String solution(int n) {
+        String answer = "";
+        for (int i = 0; i < n; i++) {
+			if(i%2==0) answer+="수";
+			else answer+="박";
+		}
+        return answer;
+    }
+}
