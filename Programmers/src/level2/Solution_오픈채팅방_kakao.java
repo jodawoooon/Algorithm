13e55e04940b7faf1086a0829493a05e0607ba31
Daun jo <jodawooooon@naver.com>
2021-09-03 22:48:19 +0900

    code : Programmers Lv2 오픈채팅방

--- Programmers/src/level2/Solution_오픈채팅방_kakao.java
+++ Programmers/src/level2/Solution_오픈채팅방_kakao.java
@@ -2,6 +2,17 @@ package level2;
 
 import java.util.*;
 
+/***
+ * 
+ * 
+ * ✨ Algorithm ✨
+ * @Problem : Programmers 오픈채팅방
+ * @Author : Daun JO
+ * @Date : 2021. 9. 3. 
+ * @Algorithm : String, HashMap
+ * @풀이과정 : https://velog.io/@jodawooooon/Java-Programmers-%EC%98%A4%ED%94%88%EC%B1%84%ED%8C%85%EB%B0%A9-2019-KAKAO-BLIND-RECRUITMENT
+ *
+ */
 public class Solution_오픈채팅방_kakao {
 	public static void main(String[] args) {
 		System.out.println(Arrays.toString(solution(new String[]{"Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan","Enter uid1234 Prodo"})));
